f12a67d41d3dfd64a6916d1c3b9094b58e20bded
Evan-C-Mead <evan.c.mead@gmail.com>
2023-04-07 16:57:30 -0500

    used generation type/object to start

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,33 +1,23 @@
 <script>
 	import { pokéPage } from './title';
-	let radNumber = 31;
-	let radDude = `${radNumber} Dude this is rad`;
-	let svltTag = '#svelte';
-	console.log('Rad Pokedex!');
-
-	const salutationsObj = {
-		 salutation: "Hey there!",
-		 name: "Friend"
-	};
-
-	const rgb = ['red', 'green', 'blue'];
+	import { generations } from './generations';
 </script>
 
 <div class="header">
 	<h1>Welcome to {pokéPage}</h1>
-	<h3 class="salutations">{salutationsObj.salutation} Nice of you to drop by, {salutationsObj.name}!</h3>
-	<h4>{radDude} <a href="https://kit.svelte.dev" target="_blank" rel="noreferrer">{svltTag}</a></h4>
-	{#each rgb as color, i (color)}
-		<h3>{i}: {color.toUpperCase()}</h3>
-	{/each}
+	<div>
+		{#each generations as generation (generation.id)}
+			<div>
+				<h2>{generation.name}</h2>
+				<p>Region: {generation.main_region}</p>
+				<p>Games: {generation.games.join(', ')}</p>
+			</div>
+		{/each}
+	</div>
 </div>
 
 <style>
 	h1 {
 		color: papayawhip;
-	}
-
-	.salutations {
-		color: aqua;
 	}
 </style>
